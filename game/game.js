60e6f521e5ed4d48b955d4111855076a4a116366
Stas <omer4d@gmail.com>
2017-04-17 15:15:00 +0300

    glbsp

--- game/game.js
+++ game/game.js
@@ -18,6 +18,7 @@ var context = canvas.getContext("2d");
 var wadStructs = (function() {
 	var char = "int8";
 	var short = "int16";
+	var ushort = "uint16";
 	var int = "int32";
 	var name = ["int8", 8, {transform: "nameToString"}];
 
@@ -50,7 +51,8 @@ var wadStructs = (function() {
 			fields: [
 				[short, ["floorHeight", "ceilHeight"]],
 				[name, ["floorTexName", "ceilTexName"]],
-				[short, ["light", "type", "tag"]]
+				[short, "light"],
+				[ushort, ["special", "tag"]]
 			]
 		},
 		{
@@ -58,23 +60,23 @@ var wadStructs = (function() {
 			fields: [
 				[short, ["xOffs", "yOffs"]],
 				[name, ["hiTexName", "lowTexName", "midTexName"]],
-				[short, "sectorIdx"]
+				[ushort, "sectorIdx"]
 			]
 		},
 		{
 			name: "LINEDEFS",
 			fields: [
-				[short, ["v1Idx", "v2Idx"]],
-				[short, ["flags", "specialFlag", "sectorTag"]],
-				[short, ["posSidedefIdx", "negSidedefIdx"]]
+				[ushort, ["v1Idx", "v2Idx"]],
+				[ushort, ["flags", "specialFlag", "sectorTag"]],
+				[ushort, ["posSidedefIdx", "negSidedefIdx"]]
 			]
 		},
 		{
 			name: "SEGS",
 			fields: [
-				[short, ["v1Idx", "v2Idx"]],
+				[ushort, ["v1Idx", "v2Idx"]],
 				[short, "angle"],
-				[short, "linedefIdx"],
+				[ushort, "linedefIdx"],
 				[short, "dir"],
 				[short, "offsOnLinedef"]
 			]
@@ -85,7 +87,30 @@ var wadStructs = (function() {
 				[short, "segNum"],
 				[short, "firstSegIdx"]
 			]
-		}
+		},
+		{
+			name: "GL_VERT",
+			fields: [
+				[int, "x"],
+				[int, "y"]
+			]
+		},
+		{
+			name: "GL_SEGS",
+			fields: [
+				[ushort, ["v1Idx", "v2Idx"]],
+				[ushort, "linedefIdx"],
+				[ushort, "dir"],
+				[ushort, "partnerSeg"]
+			]
+		},
+		{
+			name: "GL_SSECT",
+			fields: [
+				[ushort, "segNum"],
+				[ushort, "firstSegIdx"]
+			]
+		},
 	];
 })();
 
@@ -102,6 +127,12 @@ BufferReader.prototype.readInt8 = function() {
 
 BufferReader.prototype.readInt16 = function() {
   var res = this.dataView.getInt16(this.pos, true);
+  this.pos += 2;
+  return res;
+};
+
+BufferReader.prototype.readUint16 = function() {
+  var res = this.dataView.getUint16(this.pos, true);
   this.pos += 2;
   return res;
 };
@@ -138,6 +169,9 @@ function genReader(struct) {
       case "int16":
         readcall = "reader.readInt16()";
         break;
+	  case "uint16":
+		readcall = "reader.readUint16()";
+		break;
       case "int32":
         readcall = "reader.readInt32()";
         break;
@@ -186,6 +220,9 @@ function structSize(struct) {
       case "int16":
         typeBytes = 2;
         break;
+	  case "uint16":
+        typeBytes = 2;
+        break;
       case "int32":
         typeBytes = 4;
         break;
@@ -222,7 +259,7 @@ readers = genReaders(wadStructs);
 
 
 var oReq = new XMLHttpRequest();
-oReq.open("GET", "/zaza.wad", true);
+oReq.open("GET", "/zaza2.wad", true);
 oReq.responseType = "arraybuffer";
 
 
@@ -254,10 +291,24 @@ oReq.onload = function (oEvent) {
 	
 	for(var i = 0; i < dirs.length; ++i) {
 		if(readers[dirs[i].name]) {
-			var n = dirs[i].lumpSize / structSize(findStruct(wadStructs, dirs[i].name));
+			reader.setPos(dirs[i].lumpOffs);
+			
+			var n;
+			
+			if(dirs[i].name === "GL_VERT") {
+				var verBytes = [reader.readInt8(), reader.readInt8(), reader.readInt8(), reader.readInt8()];
+				var ver = String.fromCharCode.apply(null, verBytes);
+				if(ver !== "gNd2")
+					throw "Unsupported glBsp version: " + ver;
+				n = (dirs[i].lumpSize - 4) / structSize(findStruct(wadStructs, dirs[i].name));
+			}
+			else {
+				n = dirs[i].lumpSize / structSize(findStruct(wadStructs, dirs[i].name));
+			}
+			
 			var lump = [];
 			lumps[dirs[i].name] = lump;
-			reader.setPos(dirs[i].lumpOffs);
+			
 			for(var j = 0; j < n; ++j) {
 				lump.push(readers[dirs[i].name](reader, ctx));
 			}
@@ -278,12 +329,40 @@ function renderLoop() {
 	
 	context.clearRect(0, 0, canvas.width, canvas.height);
 	
+	/*
 	for(i = 0; i < lumps.SEGS.length; ++i) {
 		var seg = lumps.SEGS[i];
 		
 		context.beginPath();
 		context.moveTo(lumps.VERTEXES[seg.v1Idx].x / 5 + 256, lumps.VERTEXES[seg.v1Idx].y / 5 + 256);
 		context.lineTo(lumps.VERTEXES[seg.v2Idx].x / 5 + 256, lumps.VERTEXES[seg.v2Idx].y / 5 + 256);
+		context.stroke();
+	}*/
+	
+	for(i = 0; i < lumps.GL_SEGS.length; ++i) {
+		var seg = lumps.GL_SEGS[i];
+		var x1, y1, x2, y2;
+		var glVertFlag = 1 << 15;
+		
+		if(seg.v1Idx & glVertFlag) {
+			x1 = lumps.GL_VERT[seg.v1Idx & ~glVertFlag].x / 65536.0;
+			y1 = lumps.GL_VERT[seg.v1Idx & ~glVertFlag].y / 65536.0;
+		}else {
+			x1 = lumps.VERTEXES[seg.v1Idx].x;
+			y1 = lumps.VERTEXES[seg.v1Idx].y;
+		}
+		
+		if(seg.v2Idx & glVertFlag) {
+			x2 = lumps.GL_VERT[seg.v2Idx & ~glVertFlag].x / 65536.0;
+			y2 = lumps.GL_VERT[seg.v2Idx & ~glVertFlag].y / 65536.0;
+		}else {
+			x2 = lumps.VERTEXES[seg.v2Idx].x;
+			y2 = lumps.VERTEXES[seg.v2Idx].y;
+		}
+		
+		context.beginPath();
+		context.moveTo(x1 / 5 + 256, y1 / 5 + 256);
+		context.lineTo(x2 / 5 + 256, y2 / 5 + 256);
 		context.stroke();
 	}
 	
