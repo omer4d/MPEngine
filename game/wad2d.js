b84f44602ee2bd1579c0f2841acc1b7942dcec85
Stas <omer4d@gmail.com>
2017-04-21 20:21:36 +0300

    wad2d bsp collisions

--- game/wad2d.js
+++ game/wad2d.js
@@ -152,7 +152,52 @@ function findCircleSubsector(lumps, idx, point, rad) {
     }
 }
 
-
+function circleVsSeg(seg, cx, cy, rad, out) {
+	var dx = seg.x2 - seg.x1;
+	var dy = seg.y2 - seg.y1;
+	var len = Math.sqrt(dx*dx + dy*dy);
+	var nx = -dy;
+	var ny = dx;
+	
+	nx /= len;
+	ny /= len;
+	
+	var vx1 = cx - seg.x1;
+	var vy1 = cy - seg.y1;
+	var dpt = dx*vx1 + dy*vy1;
+	
+	if(dpt >= 0 && dpt < dx*dx+dy*dy) {
+		var dpn = nx*vx1 + ny*vy1;
+		var mtd1 = rad - dpn;
+		var mtd2 = -rad - dpn;
+		var mtd = dpn > 0 ? mtd1 : mtd2;
+		out.mtx = nx * mtd;
+		out.mty = ny * mtd;
+		out.nx = nx;
+		out.ny = ny;
+		return Math.abs(dpn) <= rad;
+	}else {
+		var vx2 = cx - seg.x2;
+		var vy2 = cy - seg.y2;
+		var d1 = vx1*vx1 + vy1*vy1;
+		var d2 = vx2*vx2 + vy2*vy2;
+		
+		if(d1 > rad*rad && d2 > rad*rad)
+			return false;
+		
+		if(d1 < d2) {
+			d1 = Math.sqrt(d1);
+			out.mtx = vx1/d1 * (rad - d1);
+			out.mty = vy1/d1 * (rad - d1);
+		}else {
+			d2 = Math.sqrt(d2);
+			out.mtx = vx2/d2 * (rad - d2);
+			out.mty = vy2/d2 * (rad - d2);
+		}
+		
+		return true;
+	}
+}
 
 
 
@@ -211,23 +256,79 @@ function renderLoop() {
 		context.fill();
 	}
 	
+	context.lineWidth = 2.0;
 	for(i = 0; i < lumps.LINEDEFS.length; ++i) {
 		var linedef = lumps.LINEDEFS[i];
 		var v1 = lumps.VERTEXES[linedef.v1Idx];
 		var v2 = lumps.VERTEXES[linedef.v2Idx];
 		line(context, v1.x, v1.y, v2.x, v2.y, "black");
 	}
+	context.lineWidth = 1.0;
 	
-	circle(context, mouseX, mouseY, 15, "red");
+	var posX = mouseX;
+	var posY = mouseY;
+	var rad = 15;
 	
+	//circle(context, posX, posY, rad, "red");
 	
-	var subs = findCircleSubsector(lumps, lumps.GL_NODES.length - 1, {x: mouseX, y: mouseY}, 15);
+	var subs = findCircleSubsector(lumps, lumps.GL_NODES.length - 1, {x: posX, y: posY}, rad);
 	
 	for(i = 0; i < subs.length; ++i) {
 		subsectorPath(context, lumps, subs[i]);
-		context.strokeStyle = "red";
+		context.strokeStyle = "blue";
 		context.stroke();
+		context.lineWidth = 3.0;
+		
+		for(var j = 0; j < subs[i].segNum; ++j) {
+			var tseg = lumps.GL_SEGS[subs[i].firstSegIdx + j];
+			if(tseg.linedefIdx !==  0xFFFF) {
+				var out = {};
+				var linedef = lumps.LINEDEFS[tseg.linedefIdx];
+				var v1 = lumps.VERTEXES[linedef.v1Idx];
+				var v2 = lumps.VERTEXES[linedef.v2Idx];
+				var seg = {x1: v1.x, y1: v1.y, x2: v2.x, y2: v2.y};
+				
+				if(circleVsSeg(seg, posX, posY, rad, out)) {
+					line(context, seg.x1, seg.y1, seg.x2, seg.y2, "red");
+				}
+			}
+		}
+		
+		context.lineWidth = 1.0;
 	}
+	
+	for(var z = 0; z < 100; ++z) {
+		var mtx = 0;
+		var mty = 0;
+		var count = 0;
+		
+		for(i = 0; i < subs.length; ++i) {
+			for(var j = 0; j < subs[i].segNum; ++j) {
+				var tseg = lumps.GL_SEGS[subs[i].firstSegIdx + j];
+				if(tseg.linedefIdx !==  0xFFFF) {
+					var out = {};
+					var linedef = lumps.LINEDEFS[tseg.linedefIdx];
+					var v1 = lumps.VERTEXES[linedef.v1Idx];
+					var v2 = lumps.VERTEXES[linedef.v2Idx];
+					var seg = {x1: v1.x, y1: v1.y, x2: v2.x, y2: v2.y};
+					
+					if(circleVsSeg(seg, posX, posY, rad, out)) {
+						mtx += out.mtx;
+						mty += out.mty;
+						++count;
+					}
+				}
+			}
+		}
+		
+		if(count > 0) {
+			posX += mtx / count;
+			posY += mty / count;
+		}
+	}
+	
+	
+	circle(context, posX, posY, rad, "red");
 	
 	
 	requestAnimationFrame(renderLoop);
